3a01a182596951182f5e1d2ca0405cddccd4bd3a
Anabelly <anabellybel123@gmail.com>
2024-06-20 14:18:56 -0300

    chart_bar3

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  const ChartBar({Key? key, 
+    required this.label,
+    required this.value,
+    required this.percentage,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-
-
-      
+    return Column(
+      children: <Widget>[
+        Text('R\$${value.toStringAsFixed(2)}'),
+        const SizedBox(height: 5),
+        const SizedBox(
+         height: 60,
+         width: 10,
+         child: null,
+        ),
+         const SizedBox(height: 5),
+         Text(label),
+      ],
     );
   }
 }
